d44a38fb66ec7879330761c2996b404e667a2799
BakijazzmanSE <bakijazman@gmail.com>
2024-05-05 16:48:23 +0100

    freestyle

---
+++ vart.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add ();
+int main ()
+{
+    printf("Welcome to Low level Programming\n");
+    printf("My name is Obafemi Temidayo, Mostly known and called Hyce\n");
+    add ();
+        return (0);
+}
+int add ()
+{
+    int p = 89;
+    int j = 74;
+    int result = p+j;
+    printf("%d\n", result);
+}
+
